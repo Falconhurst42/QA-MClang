6c5e055e51663aedaa89c34f3861d4904dab0791
Ethan Worth <eworth23@my.whitworth.edu>
2022-10-12 17:02:02 -0700

    util.h refactor

--- tests/ethan/MCLangTests.cpp
+++ tests/ethan/MCLangTests.cpp
@@ -2,142 +2,82 @@
 #include "../universal/utils.h"
 #include <fstream>
 
-const std::string DEF_NAMESPACE = "dp",
-    TEST_NAMESPACE = "gtest",
-    NAMESPACER = "namespace " + TEST_NAMESPACE + ";\n\n",
-    HELLO_WORLD = std::string("") +
-    "void main() {\n" +
-    "    /say Hello world!\n" +
-    "}";
+const SourceFile::Function HELLO_WORLD = {
+    "void",
+    "hello_world",
+    std::vector<SourceFile::Variable>(),
+    "    /say Hello world!"
+};
 
-// verifies that basic file builds with successful return code
-TEST(MCLangTests, HelloWorld_BuildSucceeds) { 
-    BuildResult br = buildFromBody(HELLO_WORLD);
-    EXPECT_EQ(br.code, 0);
-    cleanupBuild(br);
-}
-// verifies that basic file builds without any output
-TEST(MCLangTests, HelloWorld_NoOutput) { 
-    BuildResult br = buildFromBody(HELLO_WORLD);
-    EXPECT_EQ(br.output, "");
-    cleanupBuild(br);
-}
-// verifies that basic file builds and creates a datapack folder
-//  assumes default namespace
-TEST(MCLangTests, HelloWorld_DatapackCreated) { 
-    BuildResult br = buildFromBody(HELLO_WORLD);
-    const std::string BASE_PATH = makeCompiledPath(br.filename);
+const SourceFile::Namespace TEST_NAMESPACE("gtest");
+
+const SourceFile HELLO_FILE = {
+    (std::vector<SourceFile::Function>){ HELLO_WORLD }
+};
+
+// verifies that file has built properly
+//  - with successful return code
+//  - without any output
+//  - datapack folder created
+void VerifyGoodBuild(const SourceFile& src) {
+    // verify success code
+    EXPECT_EQ(src.rez.code, 0);
+    // verify no output
+    EXPECT_EQ(src.rez.output, "");
+
+    // verify file structure
+    const std::string BASE_PATH = src.getCompiledPath();
     EXPECT_TRUE(directoryExists(BASE_PATH));
     EXPECT_TRUE(directoryExists(BASE_PATH + "/data"));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + DEF_NAMESPACE));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + DEF_NAMESPACE + "/functions"));
+    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + src.getPrimaryNamespace()));
+    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + src.getPrimaryNamespace() + "/functions"));
     EXPECT_TRUE(directoryExists(BASE_PATH + "/data/minecraft"));
     EXPECT_TRUE(fileCanOpen(BASE_PATH + "/pack.mcmeta"));
-    cleanupBuild(br);
+}
+
+TEST(MCLangTests, VerifyBuild_HelloWorld) {
+    SourceFile src(HELLO_FILE);
+    src.build();
+    VerifyGoodBuild(src);
+
+    // built files cleaned up by destructor when src falls out of scope
 }
 
 // verifies that basic file builds with successful return code when provided -n arg
-TEST(MCLangTests, HelloWorld_DashN_BuildSucceeds) { 
-    BuildResult br = buildFromBody(HELLO_WORLD, TEST_NAMESPACE);
-    EXPECT_EQ(br.code, 0);
-    cleanupBuild(br);
-}
-// verifies that basic file builds without any output when provided -n arg
-TEST(MCLangTests, HelloWorld_DashN_NoOutput) { 
-    BuildResult br = buildFromBody(HELLO_WORLD, TEST_NAMESPACE);
-    EXPECT_EQ(br.output, "");
-    cleanupBuild(br);
-}
-// verifies that basic file builds and creates a datapack folder when provided -n arg
-//  checks for proper path given namespace
-TEST(MCLangTests, HelloWorld_DashN_DatapackCreated) { 
-    BuildResult br = buildFromBody(HELLO_WORLD, TEST_NAMESPACE);
-    const std::string BASE_PATH = makeCompiledPath(br.filename);
-    EXPECT_TRUE(directoryExists(BASE_PATH));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data"));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + TEST_NAMESPACE));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + TEST_NAMESPACE + "/functions"));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/minecraft"));
-    EXPECT_TRUE(fileCanOpen(BASE_PATH + "/pack.mcmeta"));
-    cleanupBuild(br);
+TEST(MCLangTests, VerifyBuild_HelloWorld_DashN) { 
+    SourceFile src(HELLO_FILE);
+    src.build(TEST_NAMESPACE);
+    VerifyGoodBuild(src);
+
+    // built files cleaned up by destructor when src falls out of scope
 }
 
 // verifies that basic file with namespace builds with successful return code
-TEST(MCLangTests, HelloWorld_Namespaced_BuildSucceeds) { 
-    BuildResult br = buildFromBody(NAMESPACER + HELLO_WORLD);
-    EXPECT_EQ(br.code, 0);
-    cleanupBuild(br);
-}
-// verifies that basic file with namespace builds without any output
-TEST(MCLangTests, HelloWorld_Namespaced_NoOutput) { 
-    BuildResult br = buildFromBody(NAMESPACER + HELLO_WORLD);
-    EXPECT_EQ(br.output, "");
-    cleanupBuild(br);
-}
-// verifies that basic file with namespace builds and creates a datapack folder
-//  checks for proper path given namespace
-TEST(MCLangTests, HelloWorld_Namespaced_DatapackCreated) { 
-    BuildResult br = buildFromBody(NAMESPACER + HELLO_WORLD);
-    const std::string BASE_PATH = makeCompiledPath(br.filename);
-    EXPECT_TRUE(directoryExists(BASE_PATH));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data"));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + TEST_NAMESPACE));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + TEST_NAMESPACE + "/functions"));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/minecraft"));
-    EXPECT_TRUE(fileCanOpen(BASE_PATH + "/pack.mcmeta"));
-    cleanupBuild(br);
+TEST(MCLangTests, VerifyBuild_HelloWorld_Namespaced) { 
+    SourceFile src(HELLO_FILE);
+    src.space = TEST_NAMESPACE;
+    src.build();
+    VerifyGoodBuild(src);
+
+    // built files cleaned up by destructor when src falls out of scope
 }
 
-// verifies that basic file with namespace builds with successful return code when also given -n arg
-TEST(MCLangTests, HelloWorld_Namespaced_DashN_BuildSucceeds) { 
-    BuildResult br = buildFromBody(NAMESPACER + HELLO_WORLD, TEST_NAMESPACE);
-    EXPECT_EQ(br.code, 0);
-    cleanupBuild(br);
-}
-// verifies that basic file with namespace builds without any output when also given -n arg
-TEST(MCLangTests, HelloWorld_Namespaced_DashN_NoOutput) { 
-    BuildResult br = buildFromBody(NAMESPACER + HELLO_WORLD, TEST_NAMESPACE);
-    EXPECT_EQ(br.output, "");
-    cleanupBuild(br);
-}
-// verifies that basic file with namespace builds and creates a datapack folder when also given -n arg
-//  checks for proper path given namespace
-TEST(MCLangTests, HelloWorld_Namespaced_DashN_DatapackCreated) { 
-    BuildResult br = buildFromBody(NAMESPACER + HELLO_WORLD, TEST_NAMESPACE);
-    const std::string BASE_PATH = makeCompiledPath(br.filename);
-    EXPECT_TRUE(directoryExists(BASE_PATH));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data"));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + TEST_NAMESPACE));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + TEST_NAMESPACE + "/functions"));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/minecraft"));
-    EXPECT_TRUE(fileCanOpen(BASE_PATH + "/pack.mcmeta"));
-    cleanupBuild(br);
+// verifies that basic file with namespace builds with successful return code when also given a concurring -n arg
+TEST(MCLangTests, VerifyBuild_HelloWorld_Namespaced_DashN) { 
+    SourceFile src(HELLO_FILE);
+    src.space = TEST_NAMESPACE;
+    src.build(TEST_NAMESPACE);
+    VerifyGoodBuild(src);
+
+    // built files cleaned up by destructor when src falls out of scope
 }
 
-// verifies that basic file with namespace builds with successful return code when also given -n arg
-TEST(MCLangTests, HelloWorld_Namespaced_DashNConflicting_BuildSucceeds) { 
-    BuildResult br = buildFromBody(NAMESPACER + HELLO_WORLD, TEST_NAMESPACE + "2");
-    EXPECT_EQ(br.code, 0);
-    cleanupBuild(br);
-}
-// verifies that basic file with namespace builds without any output when also given -n arg
-TEST(MCLangTests, HelloWorld_Namespaced_DashNConflicting_NoOutput) { 
-    BuildResult br = buildFromBody(NAMESPACER + HELLO_WORLD, TEST_NAMESPACE + "2");
-    EXPECT_EQ(br.output, "");
-    cleanupBuild(br);
-}
-// verifies that basic file with namespace builds and creates a datapack folder when also given -n arg
-//  checks that namespace in the file trumps the command line arg namespace
-TEST(MCLangTests, HelloWorld_Namespaced_DashNConflicting_DatapackCreated) { 
-    BuildResult br = buildFromBody(NAMESPACER + HELLO_WORLD, TEST_NAMESPACE + "2");
-    const std::string BASE_PATH = makeCompiledPath(br.filename);
-    EXPECT_TRUE(directoryExists(BASE_PATH));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data"));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + TEST_NAMESPACE));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/" + TEST_NAMESPACE + "/functions"));
-    EXPECT_TRUE(directoryExists(BASE_PATH + "/data/minecraft"));
-    EXPECT_TRUE(fileCanOpen(BASE_PATH + "/pack.mcmeta"));
-    cleanupBuild(br);
+// verifies that basic file with namespace builds with successful return code when also given a conflicting -n arg
+TEST(MCLangTests, VerifyBuild_HelloWorld_Namespaced_DashNConflicting) { 
+    SourceFile src(HELLO_FILE);
+    src.space = TEST_NAMESPACE;
+    src.build(TEST_NAMESPACE.space + "2");
+    VerifyGoodBuild(src);
 }
 
 // verifies that attempting to build non-existent file will fail 
@@ -148,9 +88,9 @@ TEST(MCLangTests, InvalidFile_BuildFails) {
     if(fileCanOpen(makeSourcePath(name))) {
         name += "f";
     }
-    BuildResult br = build(name);
+    CommandResult br = buildFile(makeSourcePath(name), makeCompiledPath(name));
     EXPECT_NE(br.code, 0);
     EXPECT_TRUE(inStr(br.output, "Could not open given file"));\
     // cleanup files
-    cleanupBuild(br);
+    cleanupBuildFiles(makeSourcePath(name), makeCompiledPath(name));
 }
--- tests/universal/utils.h
+++ tests/universal/utils.h
@@ -7,6 +7,7 @@
 #include <memory>
 #include <stdexcept>
 #include <array>
+#include <vector>
 #include <algorithm>
 #include <fstream>
 #include <cassert>
@@ -14,26 +15,51 @@
 
 /***************************************|
 |                                       |
-|              Path Utils               |
+|                Params                 |
+|                                       |
+|***************************************/
+const std::string DEF_FILENAME = "source";
+const std::string SRC_EXT = ".mcl",
+                  SOURCE_PATH = "../files/sourcefiles/",
+                  DATA_PATH = "../files/datafiles/";
+const std::string NO_NAMESPACE = "",
+                  DEF_NAMESPACE = "dp";
+
+/***************************************|
+|                                       |
+|            Path Generators            |
+|                                       |
+|***************************************/
+// get location of `.mcl` file
+inline std::string makeSourcePath(std::string filename) {
+    return SOURCE_PATH + filename + SRC_EXT;
+}
+// get location of datapack
+inline std::string makeCompiledPath(std::string filename) {
+    return DATA_PATH + filename;
+}
+// get location of function .json files (for load, tick)
+inline std::string makeTagsPath(std::string filename, std::string namesp) {
+    return makeCompiledPath(filename) + "/data/" + namesp + "/minecraft/tags/functions";
+}
+// get location of generated `.mcfunction` files
+inline std::string makeFunctionsPath(std::string filename, std::string namesp) {
+    return makeCompiledPath(filename) + "/data/" + namesp + "/functions";
+}
+
+/***************************************|
+|                                       |
+|             File Checkers             |
 |                                       |
 |***************************************/
 
 struct stat info;
-
-const std::string SRC_EXT = ".mcl",
-                  SOURCE_PATH = "../files/sourcefiles/",
-                  DATA_PATH = "../files/datafiles/", 
-                  DEF_FILENAME = "gtest";
-
-std::string makeSourcePath(std::string filename) {
-    return SOURCE_PATH + filename + SRC_EXT;
-}
-std::string makeCompiledPath(std::string filename) {
-    return DATA_PATH + filename;
-}
+// Checks if the file with the given path can be opened by an ifstream object
+// TODO: rewrite with <sys/stat.h>
 inline bool fileCanOpen(std::string path) {
     return std::ifstream(path).is_open();
 }
+// Checks if the given path points to a valid directory
 // https://stackoverflow.com/a/18101042
 bool directoryExists(std::string path) {
     if( stat( path.c_str(), &info ) != 0 )
@@ -69,13 +95,6 @@ struct CommandResult {
     CommandResult(std::string ou, int co) : output(ou), code(co) {}
 };
 
-// specific struct for storing results of a MCLang build
-struct BuildResult : CommandResult {
-    std::string filename;
-
-    BuildResult(CommandResult cr, std::string fi = "") : CommandResult(cr.output, cr.code), filename(fi) {}
-};
-
 // executes the given command and returns the result
 //  Getting command output: https://stackoverflow.com/a/478960
 //  Getting command result: https://stackoverflow.com/a/52165057
@@ -92,51 +111,220 @@ CommandResult execCommand(std::string cmd) {
     return CommandResult(result, pclose(pipe));
 }
 
-// builds the file with the given name and returns the result
+// builds the file with the given path and returns the result
 //  optional args for output folder and namespace command line arguments
 //  to skip -o but provide -n, use `build(fff, "", nnn)`
-BuildResult build(std::string filename, std::string o = "", std::string n = "") {
+CommandResult buildFile(std::string srcpath, std::string o = "", std::string n = NO_NAMESPACE) {
 #ifdef _WIN32
-    std::string cmd = "..\\..\\build\\main.exe " + makeSourcePath(filename);
+    std::string cmd = "..\\..\\build\\main.exe " + srcpath;
 #else
-    std::string cmd = "../../build/main " + makeSourcePath(filename);
+    std::string cmd = "../../build/main " + srcpath;
 #endif
     if(o != "")
         cmd.append(" -o " + o);
-    if(n != "")
+    if(n != NO_NAMESPACE)
         cmd.append(" -n " + n);
-    BuildResult out = execCommand(cmd);
-    out.filename = filename;
-    return out;
+    return execCommand(cmd);
 }
 
-// creates and builds a file with the given text as body
-//  places file at `makeSourcePath(filename)`
-//  outputs datapack to `makeCompiledPath(filename)`
-//  optional -n argument for namespace
-BuildResult buildFromBody(std::string filebody, std::string n = "", const std::string filename = DEF_FILENAME) {
-    // make file
-    std::ofstream f(makeSourcePath(filename));
-    if(!f.is_open())
-        throw std::runtime_error("Could not create file: " + makeSourcePath(filename));
-    
-    f << filebody;
-    f.close();
-
-    // run build
-    return build(filename, makeCompiledPath(filename), n);
-}
-
-// cleans up a given build
-//  assumes source file at `makeSourcePath(filename)`
-//  assumes datapack at `makeCompiledPath(filename)`
-//  assumes that if rez.code != 0, no datapack was created
-void cleanupBuild(BuildResult rez) {
+void cleanupBuildFiles(std::string sourcepath, std::string outpath) {
     // remove source file (if it exists)
-    if(fileCanOpen(makeSourcePath(rez.filename)))
-        system(("rm " + makeSourcePath(rez.filename)).c_str());
+    if(fileCanOpen(sourcepath))
+        system(("rm " + sourcepath).c_str());
 
-    // remove compoiled pack
-    if(rez.code == 0)
-        system(("rm -r " + makeCompiledPath(rez.filename)).c_str());
+    // remove compiled pack
+    if(directoryExists(outpath))
+        system(("rm -r " + outpath).c_str());
 }
+
+/***************************************|
+|                                       |
+|                Types                  |
+|                                       |
+|***************************************/
+
+struct SourceFile {
+/***************************************|
+|                                       |
+|              Sub-Types                |
+|                                       |
+|***************************************/
+    struct Variable {
+        std::string type, name;
+
+        std::string getDeclaration() const {
+            return type + " " + name;
+        }
+    };
+
+    struct Function {
+        std::string rtype, name;
+        std::vector<Variable> args;
+        std::string body;
+
+        Function(std::string rt, std::string na, std::vector<Variable> ar, std::string bo) :
+            rtype(rt), name(na), args(ar), body(bo) {}
+
+        std::string getText() const {
+            std::string out("");
+            out += rtype + " " + name + "(";
+            for(const Variable& a : args)
+                out += a.getDeclaration() + ", ";
+            if(!args.empty()) {
+                out.pop_back(); out.pop_back();
+            }
+            out += ") {\n" + body + "\n}";
+            return out;
+        }
+    };
+
+    struct Namespace {
+        std::string space;
+
+        Namespace(std::string sp) : space(sp) {}
+
+        std::string getNamespacer() const {
+            return "namespace " + space + ";";
+        }
+
+        operator std::string() const { return space; }
+    };
+
+    struct BuildResult : CommandResult {
+        std::string dash_n; // necessary to handle -n args
+
+        BuildResult(CommandResult cr, std::string dn = NO_NAMESPACE) : CommandResult(cr), dash_n(dn) {}
+        BuildResult(std::string ou, int co, std::string dn = NO_NAMESPACE) : CommandResult(ou, co), dash_n(dn) {}
+    };
+
+/***************************************|
+|                                       |
+|           Member Variables            |
+|                                       |
+|***************************************/
+    std::string name;
+    Namespace space;
+    std::vector<Function> foos;
+    std::vector<Variable> vars;
+    BuildResult rez;            // code == -1 means no current build
+                                // any other code means a build currently exists
+
+/***************************************|
+|                                       |
+|              Constructor              |
+|                                       |
+|***************************************/
+    SourceFile(std::vector<Function> fs = std::vector<Function>(), 
+        std::vector<Variable> vs = std::vector<Variable>(),
+        Namespace sp = NO_NAMESPACE) :
+            name(DEF_FILENAME),
+            space(sp),
+            foos(fs),
+            vars(vs),
+            rez("", -1, NO_NAMESPACE) {
+                rez.code = -1;
+            }
+    
+/***************************************|
+|                                       |
+|           Path Generators             |
+|                                       |
+|***************************************/
+    // get location of `.mcl` file
+    std::string getSourcePath() const {
+        return makeSourcePath(name);
+    }
+    // get location of datapack
+    std::string getCompiledPath() const {
+        return makeCompiledPath(name);
+    }
+    // get location of function .json files (for load, tick)
+    std::string getTagsPath() const {
+        return makeTagsPath(name, space);
+    }
+    // get location of generated `.mcfunction` files
+    std::string getFunctionsPath() const {
+        return makeFunctionsPath(name, space);
+    }
+    
+/***************************************|
+|                                       |
+|         Parsing & Generation          |
+|                                       |
+|***************************************/
+    std::string generateText() const {
+        std::string out("");
+        if(space.space != NO_NAMESPACE) {
+            out += space.getNamespacer() + "\n\n";
+        }
+        for(const Variable& v : vars) {
+            out += v.getDeclaration() + ";\n";
+        }
+        for(const Function& f : foos) {
+            out += f.getText() + "\n\n";
+        }
+        return out;
+    }
+
+    // determines the effective namespace. Accounts for in-file namespace and dash_n args of last build
+    std::string getPrimaryNamespace() const {
+        // in-file namespace trumps
+        if(space.space != NO_NAMESPACE)
+            return space;
+        // check for dash_n arg from last build
+        if(rez.code != -1 && rez.dash_n != NO_NAMESPACE)
+            return rez.dash_n;
+        // default
+        return DEF_NAMESPACE;
+    }
+
+/***************************************|
+|                                       |
+|               Building                |
+|                                       |
+|***************************************/
+    // builds the Sourcefile and returns whether the build returned a successful code
+    //  saves command result to Rez
+    //  places file at `makeSourcePath(name)`
+    //  outputs datapack to `makeCompiledPath(name)`
+    //  optional -n argument for namespace
+    bool build(std::string n = NO_NAMESPACE) {
+        // make file
+        std::ofstream f(getSourcePath());
+        if(!f.is_open())
+            throw std::runtime_error("Could not create file: " + getSourcePath());
+        
+        // pass text
+        f << generateText();
+        f.close();
+
+        // cleanup previous build
+        cleanupBuild();
+
+        // run build
+        rez = BuildResult(buildFile(getSourcePath(), getCompiledPath(), n), n);
+
+        // return success
+        return rez.code == 0;
+    }
+
+    // cleans up a given build
+    void cleanupBuild() {
+        if(rez.code != -1) {
+            // clean up files
+            cleanupBuildFiles(getSourcePath(), getCompiledPath());
+            
+            // mark as cleaned
+            rez.code = -1;
+        }
+    }
+
+/***************************************|
+|                                       |
+|              Destructor               |
+|                                       |
+|***************************************/
+    ~SourceFile() {
+        cleanupBuild();
+    }
+};
